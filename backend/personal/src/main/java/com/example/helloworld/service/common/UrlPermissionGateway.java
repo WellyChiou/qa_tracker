8791ef2f217e38974bfe9eb1aed38f2d70274bb6
WellyChiou <chiou713@gmail.com>
2026-03-17 11:14:26 +0800

    refactor(personal): remove duplicated auth contracts moved to auth-core

--- backend/personal/src/main/java/com/example/helloworld/service/common/UrlPermissionGateway.java
+++
@@ -1,9 +0,0 @@
-package com.example.helloworld.service.common;
-
-import java.util.List;
-
-public interface UrlPermissionGateway {
-    AuthDomain domain();
-
-    List<CommonUrlPermission> getActivePermissions();
-}
